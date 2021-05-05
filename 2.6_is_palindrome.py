554258d18bf71043f3b1c75d21c4131b37195030
Ian Cossentino <icossentino@gmail.com>
2021-05-05 09:06:00 -0400

    2.6

--- 2.6_is_palindrome.py
+++ 2.6_is_palindrome.py
@@ -1,26 +1,47 @@
 from linked_list_obj import Node, LinkedList
 
 n1 = Node(5)
-n2 = Node(8)
+n2 = Node(6)
 n3 = Node(7)
 n4 = Node(6)
+n5 = Node(5)
 
 n1.next = n2
 n2.next = n3
 n3.next = n4
-l1 = LinkedList(n1)
+n4.next = n5
 
 
-def reverse_and_clone(l_list):
-  head = None
-  
+def reverse_and_clone(head_node):
+  curr_0 = head_node
+  curr = Node(curr_0.val)
+  prev = None
+  nextt = None
+  while curr_0.next:
+    nextt = Node(curr_0.next.val)
+    nextt.next = curr
+    prev = curr
+    curr = nextt
+    curr_0 = curr_0.next
+  return curr
+
+def is_equal(head1, head2):
+  curr1 = head1
+  curr2 = head2
+  while curr1.next and curr2.next:
+    if curr1.val != curr2.val:
+      return False
+    curr1 = curr1.next
+    curr2 = curr2.next
+  if curr1.next or curr2.next:
+    return False
+  return True
 
 
-reverse_and_clone(l1)
+def is_palindrome(head_node):
+  rev_list_head = reverse_and_clone(head_node)
+  return True if is_equal(head_node, rev_list_head) else False
 
 
 
-
-
-
-  # reverse_list.head = reverse_list.head.next
+print(is_palindrome(n1))
